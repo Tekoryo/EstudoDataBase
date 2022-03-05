f56720882e57813469c1e177bd1f5fa3148a523c
Thiago <thiagocoelho.coelho2@gmail.com>
2022-03-05 12:42:44 -0300

    mudando nome do banco1000 para perfomace e excluindo itens desnecerarios

---
+++ Performace.sql
@@ -0,0 +1,55 @@
+/* exercicio */
+
+/* Considerando o que vc aprendeu sobre performance, resolva os exercicios abaixo */
+
+SELECT COUNT(*) FROM funcionarios;
+
+--Traga os funcionarios que trabalhem
+--no departamento de filmes OU no
+--departamento de roupas
+----------------------------------------------------------------------------------------------------
+--O gestor de marketing pediu a lista das funcionarias (AS) = FEMININO que trabalhem no departamento 
+--de filmes ou no departamento lar. Ele necessita enviar um email para as colaboradoras
+--desses dois setores. OR +  AND *
+
+--Feminino 491 ocorrencias
+select count(*), sexo
+from funcionarios
+group by sexo;
+
+--52 | Lar  21 Filmes
+select count(*), departamento
+from funcionarios
+group by departamento;
+
+SELECT * FROM FUNCIONARIOS
+WHERE
+( DEPARTAMENTO = 'Lar' AND SEXO = 'Feminino' )
+OR
+( DEPARTAMENTO = 'Filmes' AND SEXO = 'Feminino' );
+
+--Traga os funcionarios do sexo masculino
+--ou os funcionarios que trabalhem no setor Jardim
+
+SELECT * FROM FUNCIONARIOS
+WHERE
+SEXO = 'Masculino' OR
+DEPARTAMENTO = 'Jardim';
+
+create database exercicio;
+
+use exercicio;
+
+create table funcionarios
+  (
+      idFuncionario integer,
+      nome varchar(100),
+      email varchar(200),
+      sexo varchar(10),
+      departamento varchar(100),
+      admissao varchar(10),
+      salario integer,
+      cargo varchar(100),
+      idRegiao int
+  );
+
